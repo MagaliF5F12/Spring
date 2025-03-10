34c28476a8b656f163d45ecb808cf56976e4ac7e
Виталий Озеров <MagaliF5F12>
2025-03-10 12:20:07 +0300

    prak1

---
+++ src/test/java/com/example/magasklad/MagaskladApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.magasklad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagaskladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
